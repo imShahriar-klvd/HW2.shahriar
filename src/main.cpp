c68e1e120d7a2681bae344775f380f2927432c54
imShahriar-klvd <imshahriar.klvd@gmail.com>
2025-03-17 19:00:40 +0330

    finishing the things needed in HW2

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,18 @@ using namespace std;
 
 int main()
 {
+        try{
         Character s("shahriar",100,50);
         s.set_skill("swimming",10);
-        Character m("mohsen",80,100);
+        Character m("mohsen",10,-100);
         m.set_skill("fighting",2);
         game g;
         g.add_character(s);
         g.add_character(m);
+        }
+        catch(exception &a)
+        {
+                cerr << a.what() << endl;
+        }
+        
 }
